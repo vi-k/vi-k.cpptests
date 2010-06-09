5340a197d3a1de598e08710a3c0100e9705a5ec7
victor dunaev (victor dunaev <victor dunaev (victor.dunaev@gmail.com)>
2010-06-09 17:48:40 +1100

    ????? int_to_str

--- a.cpp
+++ a.cpp
@@ -1,24 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
 #include <string>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-int main()
+#define MUL10( a ) (((a) << 3 ) + ((a) << 1 ))
+
+char* i_to_s(int v, char *str)
 {
-	string str("string string string");
+	static const char sym[]
+		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-	char *new_str = new char[str.size()];
-	copy(str.begin(), str.end(), new_str);
-	cout << str << endl;
-	cout << new_str << endl;
-	delete[] new_str;
+    if (v < 0)
+    {
+        *str++ = '-';
+        //v = -v;
+    }
 
-	wstring wstr(L"wstring wstring wstring");
-	wchar_t *new_wstr = new wchar_t[wstr.size()];
-	copy(wstr.begin(), wstr.end(), new_wstr);
-	wcout << wstr << endl;
-	wcout << new_wstr << endl;
-	delete[] new_wstr;
-		
-	return 0;
+	char *ptr = str;
+
+	while (v)
+	{
+		int n = v / 10;
+		int o = v - (n<<3) - (n<<1); /* -(n * 10) */
+		*ptr++ = sym[o];
+		v = n;
+	}
+
+	*ptr-- = 0;
+
+    while (str < ptr)
+   	{
+        char tmp = *ptr;
+        *ptr-- = *str;
+        *str++ = tmp;
+    }
+
+	return ptr;
+}
+
+void itoa10( int a, char *s )
+{
+    unsigned int b = a;
+    char *p;
+
+    if ( a < 0 )
+    {
+        *s++ = '-';
+        b = -a;
+    }
+
+    p = s;
+
+    do
+    {
+        unsigned int d = ( b * 0x6667 ) >> 18; // magic
+        *p++ = b - MUL10( d ) + '0';
+        b = d;
+    }
+    while ( b );
+
+    *p-- = '\0';
+
+    // inverse
+    while ( s < p ) 
+        *p-- ^= *s++ ^= *p ^= *s;
+}
+
+int main(void)
+{
+    char buf[20];
+    itoa10(123456789, buf);
+    cout << buf << endl;
+
+    i_to_s(2147483647, buf);
+    cout << buf << endl;
+
+    i_to_s(-2147483647, buf);
+    cout << buf << endl;
+
+    return 0;
 }
--- karma.cpp
+++ karma.cpp
@@ -23,28 +23,8 @@ using namespace boost::posix_time;
 using namespace std;
 using namespace boost::spirit;
 
-#define MAX_ITERATION 1000000
+#define MAX_ITERATION 10000000
 
-
-template <typename OutputIterator>
-struct escaped_string
-  : karma::grammar<OutputIterator, std::string(char const*)>
-{
-    escaped_string()
-      : escaped_string::base_type(esc_str)
-    {
-        esc_char.add('\a', "\\a")('\b', "\\b")('\f', "\\f")('\n', "\\n")
-                    ('\r', "\\r")('\t', "\\t")('\v', "\\v")
-                    ('\'', "\\\'")('"', "\\\"")
-            ;
-        esc_str =   karma::lit(karma::_r1)
-                << *(esc_char | karma::print | "\\x" << karma::hex)
-                <<  karma::lit(karma::_r1)
-            ;
-    }
-    karma::rule<OutputIterator, std::string(char const*)> esc_str;
-    karma::symbols<char, char const*> esc_char;
-};
 
 ///////////////////////////////////////////////////////////////////////////////
 struct random_fill
@@ -55,6 +35,134 @@ struct random_fill
         return (std::rand() * std::rand()) / scale;
     }
 };
+
+#define MUL10( a ) (((a) << 3 ) + ((a) << 1 ))
+
+char* i_to_s(int v, char *str)
+{
+	static const char sym[]
+		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    if (v < 0)
+    {
+        *str++ = '-';
+        v = -v;
+    }
+
+	char *ptr = str;
+
+	while (v)
+	{
+		int n = v / 10;
+		int o = v - (n<<3) - (n<<1); /* -(n * 10) */
+		*ptr++ = sym[o];
+		v = n;
+	}
+
+	*ptr-- = 0;
+
+    while (str < ptr)
+   	{
+        char tmp = *ptr;
+        *ptr-- = *str;
+        *str++ = tmp;
+    }
+
+	return ptr;
+}
+
+char* z_itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+	
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+	
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+	
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
+
+
+void itoa10_2( short a, char *s )
+{
+	static const char sym[]
+		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    unsigned int b = a;
+    char *p;
+
+    if ( a < 0 )
+    {
+        *s++ = '-';
+        b = -a;
+    }
+
+    p = s;
+
+    do
+    {
+        unsigned int d = ( b * 0x6667 ) >> 18; // magic
+        *p++ = sym[ b - MUL10( d ) ];
+        b = d;
+    }
+    while ( b );
+
+    *p-- = '\0';
+
+    // inverse
+    //while ( s < p ) 
+    //    *p-- ^= *s++ ^= *p ^= *s;
+
+    while ( s < p ) 
+    {
+        char t = *p;
+        *p-- = *s;
+        *s++ = t;
+        //*p-- ^= *s++ ^= *p ^= *s;
+    }
+}
+
+void itoa10( short a, char *s )
+{
+    unsigned int b = a;
+    char *p;
+
+    if ( a < 0 )
+    {
+        *s++ = '-';
+        b = -a;
+    }
+
+    p = s;
+
+    do
+    {
+        unsigned int d = ( b * 0x6667 ) >> 18; // magic
+        *p++ = b - MUL10( d ) + '0';
+        b = d;
+    }
+    while ( b );
+
+    *p-- = '\0';
+
+    // inverse
+    while ( s < p ) 
+        *p-- ^= *s++ ^= *p ^= *s;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 int main()
@@ -68,28 +176,64 @@ int main()
     std::vector<int> v (MAX_ITERATION);
     std::generate(v.begin(), v.end(), random_fill()); // randomly fill the vector
 
-    // test the C libraries ltoa function (the most low level function for
-    // string conversion available)
+    /* itoa */
     {
-        //[karma_int_performance_ltoa
-        char buffer[65]; // we don't expect more than 64 bytes to be generated here
-        //<-
+        char buffer[65];
         std::string str;
 		ptime start = microsec_clock::local_time();
-        //->
         for (int i = 0; i < MAX_ITERATION; ++i)
         {
-            _itoa_s(v[i], buffer, 10);
-            //<-
-            str = buffer;      // compensate for string ops in other benchmarks
-            //->
+            itoa(v[i], buffer, 10);
+            //str = buffer;      // compensate for string ops in other benchmarks
         }
-        //]
 
 		time_duration time = microsec_clock::local_time() - start;
-        cout << "ltoa:\t\t" << to_simple_string(time) << endl;
+        cout << "itoa:\t\t" << to_simple_string(time) << endl;
     }
 
+    /* i_to_s */
+    {
+        char buffer[65];
+        std::string str;
+		ptime start = microsec_clock::local_time();
+        for (int i = 0; i < MAX_ITERATION; ++i)
+        {
+            i_to_s(v[i], buffer);
+            //str = buffer;      // compensate for string ops in other benchmarks
+        }
+		time_duration time = microsec_clock::local_time() - start;
+        cout << "i_to_s:\t\t" << to_simple_string(time) << endl;
+    }
+
+    /* itoa10 */
+    {
+        char buffer[65];
+        std::string str;
+		ptime start = microsec_clock::local_time();
+        for (int i = 0; i < MAX_ITERATION; ++i)
+        {
+            itoa10(v[i], buffer);
+            //str = buffer;      // compensate for string ops in other benchmarks
+        }
+		time_duration time = microsec_clock::local_time() - start;
+        cout << "itoa10:\t\t" << to_simple_string(time) << endl;
+    }
+
+    /* itoa10 */
+    {
+        char buffer[65];
+        std::string str;
+		ptime start = microsec_clock::local_time();
+        for (int i = 0; i < MAX_ITERATION; ++i)
+        {
+            itoa10_2(v[i], buffer);
+            //str = buffer;      // compensate for string ops in other benchmarks
+        }
+		time_duration time = microsec_clock::local_time() - start;
+        cout << "itoa10_2:\t\t" << to_simple_string(time) << endl;
+    }
+
+    #if 0
     // test the iostreams library
     {
         //[karma_int_performance_iostreams
@@ -125,29 +269,27 @@ int main()
 		time_duration time = microsec_clock::local_time() - start;
         cout << "Boost.Format:\t" << to_simple_string(time) << endl;
     }
+    #endif
 
     // test the Karma int_ generation routines 
     {
         std::string str;
 		ptime start = microsec_clock::local_time();
 
-        //[karma_int_performance_plain
         char buffer[65]; // we don't expect more than 64 bytes to be generated here
         for (int i = 0; i < MAX_ITERATION; ++i)
         {
             char *ptr = buffer;
             karma::generate(ptr, int_, v[i]);
             *ptr = '\0';
-            //<-
-            str = buffer;      // compensate for string ops in other benchmarks
-            //->
+            //str = buffer;      // compensate for string ops in other benchmarks
         }
-        //]
 
 		time_duration time = microsec_clock::local_time() - start;
         cout << "int_:\t\t" << to_simple_string(time) << endl;
     }
 
+    #if 0
     // test the Karma int_ generation routines 
     {
 	    typedef std::back_insert_iterator<std::string> sink_type;
@@ -166,21 +308,7 @@ int main()
 		time_duration time = microsec_clock::local_time() - start;
         cout << "int_:\t\t" << to_simple_string(time) << endl;
     }
-
-    {
-	    typedef std::back_insert_iterator<std::string> sink_type;
- 
-		std::string generated;
-		sink_type sink(generated);
- 
-		std::string str("string to escape: \n\r\t\"'\x19");
-		char const* quote = "\'\'\'";
- 
-		escaped_string<sink_type> g;
-		karma::generate(sink, g(quote), str);
-		cout << generated << endl;
-		// this will emit: ’’’string to escape: \n\r\t\"\'\x19’’’</a>
-    }
+    #endif
 
     return 0;
 }
